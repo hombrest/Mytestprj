c5ea9aafe90ad61ba4a41016730031e14a09b4ab
dev <dev@abc.com>
2025-02-06 21:15:41 +0800

    enhance extraction job

--- My_library/job_extraction.py
+++ My_library/job_extraction.py
@@ -4,45 +4,45 @@ import pandas as pd
 import time
 import re
 
-from tabulate import tabulate
+# from tabulate import tabulate
 
 
-def extract_job_detail(url, with_header=False):
+def extract_job_detail(job_url):
     try:
         # response = requests.get(url, headers=headers)
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
+        job_response = requests.get(job_url, timeout=10)
+        if job_response.status_code == 200:
+            _soup = BeautifulSoup(job_response.text, 'html.parser')
 
-            form = soup.find("form", {"name": "jobForm"})
+            form = _soup.find("form", {"name": "jobForm"})
 
             if not form:
                 print("Form named 'jobForm' not found.")
             else:
                 # Extract field names and corresponding data
                 # fields = {}
-                header = []
-                data = []
-                rows = form.find_all("tr")  # Assuming the form uses <tr> for rows
-                for row in rows:
+                
+                data = {}
+                job_rows = form.find_all("tr")  # Assuming the form uses <tr> for rows
+                for row in job_rows:
                     columns = row.find_all("td")  # Assuming the form uses <td> for columns
                     if len(columns) == 2:  # Two columns: field name and data
                         field_name = columns[0].text.strip()
                         field_data = columns[1].text.strip()
                         if field_name and field_name not in ['Monthly Salary Range HK$', 'Payroll', 'Apply To', 'Direct Line', 'Employer Business']:
-                            # fields[field_name] = field_data
-                            if with_header:
-                                header.append(field_name)
-                            data.append(field_data)
-
+                            # fields[field_name] = field_data                           
+                                data[field_name] = field_data
+                                
+                                if field_name == 'Duties':
+                                	data['B/D'] = extract_bd(field_data)
+                                if  field_name == 'Job Title/ Category' :
+                                	data['Title'] = extract_title(field_data)                                                        
                 # Print the extracted data
                 # for field_name, field_data in fields.items():
                 #     print(f"{field_name}: {field_data}")
                 # print(json.dumps(fields, indent=2))
-                if with_header:
-                    return [header, data]
-                else:
-                    return data
+                
+                return data
 
         else:
             print(f"Failed to fetch page. Status code: {response.status_code}")
@@ -80,7 +80,7 @@ def extract_bd(text):
 
 url = "https://infotech.com.hk/itjs/job/fe-search.do?method=feList&sortByField=jjm_activedate&sortByOrder=DESC"
 
-search_keys = []
+search_keys = set()
 try:
     response = requests.get(url)
     if response.status_code == 200:
@@ -93,39 +93,35 @@ try:
                 sub_url = link.get('href')
                 key_pos = sub_url.index('jjKey=')
                 if key_pos >= 0:
-                    search_keys.append(sub_url[key_pos + len('jjKey='):])
+                    search_keys.add(sub_url[key_pos + len('jjKey='):])
                 # print(f"Link: {link.text.strip()} -> {sub_url}")
-
+         
+        file_path = 'job_detail.xlsx'
+        sheet_name = 'Details'
+        df = pd.read_excel(file_path, sheet_name=sheet_name)
         print("Start job detail extraction")
         base_url = "https://www.infotech.com.hk/itjs/job/fe-view.do?method=feView&jjKey="
 
+        rows = []
         start = time.time()
-        job = extract_job_detail(base_url + search_keys[0], with_header=True)
-        rows = [*job]
-        end = time.time()
-        print(f"Extract header and the first job in {round(end - start, 2)} seconds.")
 
-        for index, key in enumerate(search_keys[1:]):
+        for key in search_keys - set(df['Job Key No'].astype(str)):
             job_start = time.time()
             job = extract_job_detail(base_url + key)
             job_end = time.time()
             if job:
                 rows.append(job)
-            print(f"Extract job {index + 1} of {len(search_keys)} in {round(job_end - job_start, 2)} seconds.")
+            print(f"Extract job {key} in {round(job_end - job_start, 2)} seconds.")
 
         end = time.time()
         print(f"Total extraction time: {round(end - start, 2)} seconds.")
-
-        df = pd.DataFrame(rows[1:], columns=rows[0])
-
-        if 'Duties' in df.columns:
-            df['B/D'] = df['Duties'].apply(extract_bd)
-            df['Title'] = df['Job Title/ Category'].apply(extract_title)
-
-        print(tabulate(df[['B/D', 'Title', 'Duties']].head(2), headers='keys', tablefmt='pretty', showindex=False,
-                       stralign='left'))
-
-        df.to_excel("job_detail.xlsx", index=False)
+        if rows:
+        	new_rows = pd.DataFrame(rows)
+        	df = pd.concat([df, new_rows], ignore_index=True)
+        	with pd.ExcelWriter(file_path, mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
+        		df.to_excel(writer, sheet_name=sheet_name, index=Flase)
+        else:
+        	print('no record found')
 
     else:
         print(f"Failed to fetch page. Status code: {response.status_code}")
